4849f32153a30d0960519c30bef5f78418bde827
Sahil Phule <sahilph@users.noreply.github.com>
2024-09-20 06:11:58 -0400

    Remove Bind address

--- sahilph-fulcrum/exports.sh
+++ sahilph-fulcrum/exports.sh
@@ -1,7 +1,7 @@
 export APP_FULCRUM_IP="10.21.22.6"
 export APP_FULCRUM_NODE_IP="10.21.21.11"
 
-export APP_FULCRUM_NODE_PORT="50002"
+export APP_FULCRUM_NODE_PORT="50001"
 
 rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
 export APP_FULCRUM_RPC_HIDDEN_SERVICE="$(cat "${rpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
